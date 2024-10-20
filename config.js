3f92d186fc6045392362f6b272c95d6d2282e692
annv <anchicken99@gmail.com>
2024-10-20 13:18:15 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Thủy';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20/10';
+const titleCard = 'Tặng Thủy ';
+const contentCard = 'Nhân dịp ngày 20/10. Chúc e luôn luôn vui vẻ và nở những nụ cười trên môi, mong điều tốt đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Chúc e ngày càng xinh xắn đáng iuuu nha';
+
+
+const giftImage = 'thuy.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
